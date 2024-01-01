edf978e4f74a30764a090fc03bc6637da593d14c
brohithkr <brohithkr@gmail.com>
2024-01-01 14:34:29 +0530

    almost finished logic

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,24 @@
 // @ts-check
 
 (async () => {
-  
   chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-    console.log()
-    console.log(msg);
-    sendResponse();
+    /**
+     * @type {Element}
+     */
+    var pdfPanel;
+    let timer = setInterval(() => {
+      pdfPanel = document.getElementsByClassName('pdf-control-panel')[0];
+      if (pdfPanel) {
+        clearTimeout(timer);
+        const pdfUrl = msg.url;
+        // alert(`${pdfPanel} ${pdfUrl}`);
+        console.log(pdfPanel);
+        pdfPanel.innerHTML += `<a href="${pdfUrl}"><button></button></a>`;
+        sendResponse();
+      }
+    }, 150);
+    // console.log()
     return undefined;
+    // console.log(msg);
   });
 })();
